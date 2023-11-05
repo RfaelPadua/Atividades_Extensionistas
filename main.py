6e538846e9fe5f577ea7565a192a2de675bdfa1c
Rafael de Padua Oliveira <rafael.padua@proton.me>
2023-11-05 17:17:34 -0300

    .

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ import random
 pygame.init()
 tela = pygame.display.set_mode((1200, 600))
 pygame.display.set_caption('Quiz')
+fonte = pygame.font.SysFont('Arial', 30, True, True)
+game_ativo = True
+
+background_surf = pygame.image.load('graficos/background.png').convert()
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.quit()
             exit()
+
+    
+    if game_ativo:
+        tela.blit(background_surf, (0, 0))
+        pygame.display.update()
