faecea85809e44cc1800e8eb9d663ee4433f81d2
Rafael de Padua Oliveira <rafael.padua@proton.me>
2023-11-18 13:39:33 -0300

    .

---
+++ Questoes.py
@@ -0,0 +1,13 @@
+
+questions = [
+    ["Qual bicho transmite Doença de Chagas?", "Barbeiro", "Barata", "Pulga", "Abelha", 1],
+    ["Qual fruto ? conhecido no Norte e Nordeste como jerimum", "Ab?bora", "Caju", "Chuchu", "C?co", 1],
+    ["Qual ? o coletivo de c?es?", "Matilha", "Rebanho", "Alcateia", "Manada", 1],
+    ["Qual ? o tri?ngulo que tem todos os lados diferentes?", "Escaleno", "Is?sceles", "Equil?tero", "Trap?zio", 1],
+    ["Quem comp?s o Hino Nacional?", "Manoel da Silva", "Manuel Bandeira", "Castro Alvez", "Dom Pedro I", 1],
+    ["Qual ? o ant?nimo de malograr?", "Conseguir", "Fracassar", "Perder", "Desprezar", 1],
+    ["Em que pa?s nasceu Carmem Miranda?", "Portugal", "Espanha", "Brasil", "Argentina", 1],
+    ["Qual foi o ?ltimo Presidente do per?odo da ditadura militar no Brasil?", "Jo?o figueiredo", "Costa e silva", "Ernesto Geisel", "Em?lio Medici", 1],
+    ["Seguindo a sequ?ncia do baralho, qual carta vem depois do dez", "Valete", "Rainha", "Rei", "11", 1],
+    ["O adjetivo venoso est? relacionado a:", "Veia", "Vento", "Veneno", "Venom", 1],
+]
--- Quiz.py
+++ Quiz.py
@@ -2,19 +2,10 @@ import pygame
 import sys
 import random
 import time
+from Questoes import questions
 
-questions = [
-    ["Qual bicho transmite Doença de Chagas?", ["Barbeiro", "Barata", "Pulga", "Abelha"]],
-    ["Qual fruto ? conhecido no Norte e Nordeste como jerimum", ["Ab?bora", "Caju", "Chuchu", "C?co"]],
-    ["Qual ? o coletivo de c?es?", ["Matilha", "Rebanho", "Alcateia", "Manada"]],
-    ["Qual ? o tri?ngulo que tem todos os lados diferentes?", ["Escaleno", "Is?sceles", "Equil?tero", "Trap?zio"]],
-    ["Quem comp?s o Hino Nacional?", ["Manoel da Silva", "Manuel Bandeira", "Castro Alvez", "Dom Pedro I"]],
-    ["Qual ? o ant?nimo de malograr?", ["Conseguir", "Fracassar", "Perder", "Desprezar"]],
-    ["Em que pa?s nasceu Carmem Miranda?", ["Portugal", "Espanha", "Brasil", "Argentina"]],
-    ["Qual foi o ?ltimo Presidente do per?odo da ditadura militar no Brasil?", ["Jo?o figueiredo", "Costa e silva", "Ernesto Geisel", "Em?lio Medici"]],
-    ["Seguindo a sequ?ncia do baralho, qual carta vem depois do dez", ["Valete", "Rainha", "Rei", "11"]],
-    ["O adjetivo venoso est? relacionado a:", ["Veia", "Vento", "Veneno", "Venom"]],
-]
+
+
 
 # Classe para criar os botões do menu
 class Botao_menu(pygame.sprite.Sprite):
@@ -52,44 +43,56 @@ class Botao_menu(pygame.sprite.Sprite):
 
 # Classe para criar os botões das respostas
 class Quiz(pygame.sprite.Sprite):
-    global alternativas_ordem, questions, fonte
+    global alternativas_ordem, questions, questao_atual
+    questao_atual = random.randint(0, 9)
 
     alternativas_ordem = [0, 1, 2, 3]
     # random.shuffle(alternativas_ordem)
     def __init__(self):
         super().__init__()
-    
-    
+    def draw_text_quiz(text, color, x, y):
+        global fonte
+        img = fonte.render(text, True, color)
+        img_rect = img.get_rect(midleft=(x, y))
+        tela.blit(img, img_rect)
 
-    def pergunta():
-        draw_text(questions[0][0], fonte, (255, 255, 255), 0, 40)
 
-    def alternativa():
-        draw_text('A. ', fonte, (255, 255, 255), 0, 300)
-        draw_text(questions[0][1][0], fonte, (255, 255, 255), 25, 300)
+    def pergunta(self):
+        self.draw_text_quiz(questions[questao_atual][0], (0, 0, 0), 0, 40)
 
-        draw_text('B. ', fonte, (255, 255, 255), 0, 350)
-        draw_text(questions[1][1][1], fonte, (255, 255, 255), 25, 350)
+    # def botao_alternativa(self):
+        
+    def alternativa(self):
+       
+        botao_alternativa = Botao_menu(40, 300, botao, 0.93, 'Matematica')
+        
+        self.draw_text_quiz('A. ', (0, 0, 0), 0, 300)
+        self.draw_text_quiz(questions[questao_atual][1], (0, 0, 0), 40, 300)
 
-        draw_text('C. ', fonte, (255, 255, 255), 0, 400)
-        draw_text(questions[1][1][2], fonte, (255, 255, 255), 25, 400)
+        self.draw_text_quiz('B. ', (0, 0, 0), 0, 350)
+        self.draw_text_quiz(questions[questao_atual][2], (0, 0, 0), 40, 350)
 
-        draw_text('D. ', fonte, (255, 255, 255), 0, 450)
-        draw_text(questions[1][1][3], fonte, (255, 255, 255), 25, 450)
-    
-    def update(self):
-        self.pergunta()
-        self.alternativa()
+        self.draw_text_quiz('C. ', (0, 0, 0), 0, 400)
+        self.draw_text_quiz(questions[questao_atual][3], (0, 0, 0), 40, 400)
+
+        self.draw_text_quiz('D. ', (0, 0, 0), 0, 450)
+        self.draw_text_quiz(questions[questao_atual][4], (0, 0, 0), 40, 450)
+
+
+    def update(self): 
+        self.pergunta(self=Quiz)
+        self.alternativa(self=Quiz)
 
         
         
 
 
-def draw_text(text, font, color, x, y):
-    img = font.render(text, True, color)
+def draw_text(text, color, x, y):
+    global fonte_outline
+    img = fonte.render(text, True, color)
     img_rect = img.get_rect(center=(x, y))
-    img_sombra = font.render(text, True, (0,0,0))
-    img_rect_sombra = img.get_rect(center=(x+2, y+2))
+    img_sombra = fonte_outline.render(text, True, (0,0,0))
+    img_rect_sombra = img.get_rect(center=(x, y))
     tela.blit(img_sombra, img_rect_sombra)
     tela.blit(img, img_rect)
 
@@ -103,14 +106,17 @@ relogio = pygame.time.Clock()
 status_jogo = 'inicio'
 materia = ''
 clicou = False
-fonte = pygame.font.SysFont('Arial', 30)
+fonte = pygame.font.SysFont('Calibri Bold', 48)
+fonte_outline = pygame.font.SysFont('Calibri Bold', 48)
 
 background_inicio = pygame.image.load('imagens/background1.png').convert()
 background_inicio = pygame.transform.scale(background_inicio, (1200, 675))
+background_quiz = pygame.image.load('imagens/background.png').convert()
+background_quiz = pygame.transform.scale(background_quiz, (1200, 675))
 botao = pygame.image.load('imagens/botao_menu2.png').convert_alpha()
 
 botao_menu = Botao_menu(600, 380, botao, 0.93, '')
-botao_voltar = Botao_menu(150, 570, botao, 0.5, '')
+botao_voltar = Botao_menu(150, 600, botao, 0.5, '')
 botao_matematica = Botao_menu(600, 380, botao, 0.93, 'Matematica')
 botao_geografia = Botao_menu(600, 485, botao, 0.93, 'Geografia')
 
@@ -135,7 +141,7 @@ while True:
             status_jogo = 'menu'
 
 
-        draw_text('Jogar', fonte, (255, 255, 255), 600, 300)
+        draw_text('Jogar', (0 ,0 , 0), 600, 380)
         pygame.display.update()
         
     elif status_jogo == 'menu':
@@ -153,17 +159,16 @@ while True:
             hit_sound.play()
             status_jogo = 'inicio'
         
-        draw_text('Matematica', fonte, (255, 255, 255), 600, 380)
-        draw_text('Geografia', fonte, (255, 255, 255), 600, 485)
-        draw_text('Voltar', fonte, (255, 255, 255), 150, 570)
+        draw_text('Matemática', (0, 0, 0), 600, 380)
+        draw_text('Geografia', (0, 0, 0), 600, 485)
+        draw_text('Voltar', (0, 0, 0), 150, 600)
         pygame.display.update()
     
     elif status_jogo == 'jogando':
-        tela.blit(background_inicio, (0, 0))
+        tela.blit(background_quiz, (0, 0))
+
 
 
         Quiz.update(self=Quiz)
-
-        
 
         pygame.display.update()
