8360bb724627498b9a80a897c3c260b0400352cb
Rafael de Padua Oliveira <rafael.padua@proton.me>
2023-11-19 21:24:47 -0300

    .

--- Quiz.py
+++ Quiz.py
@@ -5,6 +5,8 @@ import time
 from Questoes import *
 
 
+materia_cont_perguntas = [0, 0]
+materia_cont_rodadas = [0, 0]
 points = 0
 
 # Classe para criar os botões do menu
@@ -43,28 +45,43 @@ class Botao_menu(pygame.sprite.Sprite):
 
 # Classe para criar os botões das respostas
 class Quiz(pygame.sprite.Sprite):
-    global alternativas_ordem, questions, questao_atual, rodadas
+    global questions, materia_cont_perguntas, materia_cont_rodadas
 
-    questao_atual = 0
-    rodadas = 0
-    
-
-    alternativas_ordem = [0, 1, 2, 3]
-    # random.shuffle(alternativas_ordem)
     def __init__(self):
         super().__init__()
+
+
     def draw_text_quiz(text, color, x, y):
-        global fonte
-        img = fonte.render(text, True, color)
+        global fonte2
+        
+        img = fonte2.render(text, True,color)
         img_rect = img.get_rect(midleft=(x, y))
         tela.blit(img, img_rect)
 
 
     def pergunta(self):
-        self.draw_text_quiz(questions[questao_atual][0], (0, 0, 0), 0, 40)
+
+        palavras_pergunta = [palavra.split(' ') for palavra in questions[materia][materia_cont_perguntas[materia]][0].splitlines()]
+        espaco = fonte.size(' ')[0]
+        x, y = 250,170
+
+        for linha in palavras_pergunta:
+            for palavra in linha:
+                palavra_superficie = fonte.render(palavra, True, (0, 0, 0))
+                palavra_largura, palavra_altura = palavra_superficie.get_size()
+                if x + palavra_largura >= 950:
+                    x = 250
+                    y += palavra_altura
+                palavra_rect = palavra_superficie.get_rect()
+                palavra_rect.midleft = (x, y)
+                tela.blit(palavra_superficie, palavra_rect)
+                x += palavra_rect.width + espaco
+
+
+        
 
     def botao_alternativa(self, x, y, image, scale):
-        global clicou
+        global clicou, materia
         width = image.get_width()
         height = image.get_height()
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
@@ -77,8 +94,8 @@ class Quiz(pygame.sprite.Sprite):
                 self.clicked = True
                 action = True
                 clicou = False
-                pygame.time.delay(100)
-
+    
+                    
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
 
@@ -86,79 +103,84 @@ class Quiz(pygame.sprite.Sprite):
         
         return action
 
-        
+    
     def alternativa(self):
-        global questao_atual, points
-        A = [200, 400]  # Coordenadas alteradas
-        B = [600, 400]  # Coordenadas alteradas
-        C = [200, 500]  # Coordenadas alteradas
-        D = [600, 500]  # Coordenadas alteradas
+        global materia_cont_perguntas, points, materia
+        A = [350, 450]  # Coordenadas alteradas
+        B = [850, 450]  # Coordenadas alteradas
+        C = [350, 625]  # Coordenadas alteradas
+        D = [850, 625]  # Coordenadas alteradas
 
         if self.botao_alternativa(self, A[0], A[1], botao, 0.7):
-            if questions[questao_atual][5] == 1:
-                hit_sound.play()
-                questao_atual += 1
+            if questions[materia][materia_cont_perguntas[materia]][5] == 1:
+                som_acertou.play()
+                materia_cont_perguntas[materia] += 1
                 points += 1
                 print(points)
             else:
-                questao_atual += 1
-                print(points)
+                materia_cont_perguntas[materia] += 1
+                som_erro.play()
+                
 
         self.draw_text_quiz('A. ', (0, 0, 0), A[0]-130, A[1])
-        self.draw_text_quiz(questions[questao_atual][1], (0, 0, 0), A[0]-90, A[1])
+        self.draw_text_quiz(questions[materia][materia_cont_perguntas[materia]][1], (0, 0, 0), A[0]-90, A[1])
 
         if self.botao_alternativa(self, B[0], B[1], botao, 0.7):  # Coordenadas alteradas
-            if questions[questao_atual][5] == 2:
-                hit_sound.play()
-                questao_atual += 1
+            if questions[materia][materia_cont_perguntas[materia]][5] == 2:
+                som_acertou.play()
+                materia_cont_perguntas[materia] += 1
                 points += 1
                 print(points)
             else:
-                questao_atual += 1
+                materia_cont_perguntas[materia] += 1
                 print(points)
+                som_erro.play()
+
 
         self.draw_text_quiz('B. ', (0, 0, 0), B[0]-130, B[1])  # Coordenadas alteradas
-        self.draw_text_quiz(questions[questao_atual][2], (0, 0, 0), B[0]-90, B[1])  # Coordenadas alteradas
+        self.draw_text_quiz(questions[materia][materia_cont_perguntas[materia]][2], (0, 0, 0), B[0]-90, B[1])  # Coordenadas alteradas
 
         if self.botao_alternativa(self, C[0], C[1], botao, 0.7):  # Coordenadas alteradas
-            if questions[questao_atual][5] == 3:
-                hit_sound.play()
-                questao_atual += 1
+            if questions[materia][materia_cont_perguntas[materia]][5] == 3:
+                som_acertou.play()
+                materia_cont_perguntas[materia] += 1
                 points += 1
                 print(points)
             else:
-                questao_atual += 1
+                materia_cont_perguntas[materia] += 1
                 print(points)
+                som_erro.play()
 
         self.draw_text_quiz('C. ', (0, 0, 0), C[0]-130, C[1])  # Coordenadas alteradas
-        self.draw_text_quiz(questions[questao_atual][3], (0, 0, 0), C[0]-90, C[1])  # Coordenadas alteradas
+        self.draw_text_quiz(questions[materia][materia_cont_perguntas[materia]][3], (0, 0, 0), C[0]-90, C[1])  # Coordenadas alteradas
 
         if self.botao_alternativa(self, D[0], D[1], botao, 0.7):  # Coordenadas alteradas
-            if questions[questao_atual][5] == 4:
-                hit_sound.play()
-                questao_atual += 1
+            if questions[materia][materia_cont_perguntas[materia]][5] == 4:
+                som_acertou.play()
+                materia_cont_perguntas[materia] += 1
                 points += 1
                 print(points)
             else:
-                questao_atual += 1
+                materia_cont_perguntas[materia] += 1
                 print(points)
+                som_erro.play()
 
         self.draw_text_quiz('D. ', (0, 0, 0), D[0]-130, D[1])  # Coordenadas alteradas
-        self.draw_text_quiz(questions[questao_atual][4], (0, 0, 0), D[0]-90, D[1])  # Coordenadas alteradas
+        self.draw_text_quiz(questions[materia][materia_cont_perguntas[materia]][4], (0, 0, 0), D[0]-90, D[1])  # Coordenadas alteradas
+
+       
 
     def pontuacao(self):
         global points
-        self.draw_text_quiz('Pontuação: ' + str(points), (0, 0, 0), 0, 600)
+        # self.draw_text_quiz('Pontuação: ' + str(points), (0, 0, 0), 0, 600)
 
     def update(self):
-        global status_jogo, points, questao_atual, rodadas
+        global status_jogo, points, materia_cont_perguntas, materia_cont_rodadas
 
-        if questao_atual - rodadas*10 == 10:
-            rodadas += 1
+        if materia_cont_perguntas[materia] - materia_cont_rodadas[materia]*10 == 10:
+            materia_cont_rodadas[materia] += 1
             status_jogo = 'fim'
         
-        if questao_atual >= len(questions):
-            questao_atual = 0
         
         self.pergunta(self=Quiz)
         self.alternativa(self=Quiz)
@@ -179,24 +201,29 @@ pygame.init()
 pygame.mixer.init()
 tela = pygame.display.set_mode((1200, 675))
 hit_sound = pygame.mixer.Sound('audios/hit.wav')
+som_acertou = pygame.mixer.Sound('audios/Som_acerto.mp3')
+som_erro = pygame.mixer.Sound('audios/Som_erro.mp3')
 pygame.display.set_caption('Quiz')
 relogio = pygame.time.Clock()
 status_jogo = 'inicio'
-materia = ''
+materia = 0
+
+points = 0
 clicou = False
-fonte = pygame.font.SysFont('Calibri Bold', 48)
-fonte_outline = pygame.font.SysFont('Calibri Bold', 48)
+fonte = pygame.font.Font('font/Grand9K_Pixel.ttf', 48)
+fonte_outline = pygame.font.Font('font/Grand9K_Pixel.ttf', 48)
+fonte2 = pygame.font.Font('font/FonteGiz.ttf',48)
 
 background_inicio = pygame.image.load('imagens/Tela_Inicial.jpg').convert()
 background_inicio = pygame.transform.scale(background_inicio, (1200, 675))
-background_quiz = pygame.image.load('imagens/background.jpg').convert()
+background_quiz = pygame.image.load('imagens/Quiz.jpg').convert()
 background_quiz = pygame.transform.scale(background_quiz, (1200, 675))
 botao = pygame.image.load('imagens/botao_menu2.png').convert_alpha()
 
-botao_menu = Botao_menu(600, 380, botao, 0.93, '')
-botao_voltar = Botao_menu(150, 600, botao, 0.5, '')
-botao_matematica = Botao_menu(600, 380, botao, 0.93, 'Matematica')
-botao_geografia = Botao_menu(600, 485, botao, 0.93, 'Geografia')
+botao_menu = Botao_menu(600, 380, botao, 0.93, 0)
+botao_voltar = Botao_menu(150, 600, botao, 0.7, 0)
+botao_matematica = Botao_menu(600, 380, botao, 0.93, 0)
+botao_geografia = Botao_menu(600, 485, botao, 0.93, 1)
 
 while True:
     tela.fill((0, 0, 0))
@@ -228,11 +255,11 @@ while True:
         if botao_matematica.draw(tela):
             hit_sound.play()
             status_jogo = 'jogando'
-            materia = 'Matematica'
+            materia = 0
         if botao_geografia.draw(tela):
             hit_sound.play()
             status_jogo = 'jogando'
-            materia = 'Geografia'
+            materia = 1
         if botao_voltar.draw(tela):
             hit_sound.play()
             status_jogo = 'inicio'
@@ -244,6 +271,11 @@ while True:
     
     elif status_jogo == 'jogando':
         tela.blit(background_quiz, (0, 0))
+        
+        print(materia)
+        for materias in materia_cont_perguntas:
+            if materia >= 50:
+                materia = 0
         
         Quiz.update(self=Quiz)
         pygame.display.update()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+for materia in materia_cont_perguntas:
+        #     if materia >= 50:
+        #         materia = 0
